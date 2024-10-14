c974854a2f1e63b6d2447b7ba92e64ad06ccaa6e
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2024-10-15 01:07:20 +0200

    Update install.hpp

--- include/install.hpp
+++ include/install.hpp
@@ -7,12 +7,26 @@
 
 #include "basic.hpp"
 
-std::pair<std::string, std::string> install();
+struct OperatingSystem {
+        std::string name;
+        std::string url;
+};
 
-bool extractArchive(const std::string& filename, const std::string& outputDirectory);
+struct InstallResult {
+    std::string name;
+    std::string url;
+    std::string filename;
+};
+
+InstallResult install(const std::string& archost);
+
+int showMenu(const std::vector<OperatingSystem>& osList);
+
 bool downloadFile(const std::string& url, const std::string& filename);
 
-void AutoCommands();
+std::pair<std::string, bool> extractArchive(const std::string& filename, const std::string& outputDirectory);
+
+void AutoCommands(const std::string& name, const std::string& outputDir);
 
 void mountChrootAndExecute(const std::string& rootfs_dir, const std::vector<MountData>& mount_list, const std::vector<std::string>& commands, const std::string& shell_path);
 
