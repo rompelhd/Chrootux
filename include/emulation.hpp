315872e44df1d90b8fcaa70b0918b5df69166f5c
Hectorete <75935831+rompelhd@users.noreply.github.com>
2026-07-10 04:17:59 +0200

    Delete include directory
    - migrating to Rust

--- include/emulation.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-bool setup_emulation(
-    const std::string& rootfs,
-    const std::string& rootfs_arch,
-    const std::string& host_arch
-);
--- include/snow-effect.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef SNOW_EFFECT_HPP
-#define SNOW_EFFECT_HPP
-
-int snowtest();
-
-#endif
